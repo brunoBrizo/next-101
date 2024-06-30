a0c20ab398cc0dd00c9d6e160216c35aa8f33606
Bruno Brizolara <bbrizolara7@gmail.com>
2024-06-30 13:04:15 -0300

    add dashboard

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # Development
+Pasos para levantar la app en desarrollo
 
-Steps to run the app in development
 
-1. Start the database
-
+1. Levantar la base de datos
 ```
 docker compose up -d
 ```
 
-2. Rename .env.template to .env
-3. Replace the environment variables
-4. Run the SEED to [create the local database](localhost:3000/api/seed)
+2. Crear una copia de el .env.template y renombrarlo a .env
+3. Reemplazar las variables de entorno
+4. Ejecutar el comando ```npm install``` para reconstruir los módulos de node
+5. Ejecutar el comando ```npm run dev``` para ejecutar aplicación en desarrollo
+6. Ejecutar estos comandos de Prisma
+```
+npx prisma migrate dev
+npx prisma generate
+```
+7. Ejecutar el SEED para [crear la base de datos local](localhost:3000/api/seed)
+
 
 # Prisma commnads
-
 ```
 npx prisma init
 npx prisma migrate dev
@@ -21,6 +27,9 @@ npx prisma generate
 
 ```
 
+
+
 # Prod
+
 
 # Stage
--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,18 +1,21 @@
-import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import prisma from '@/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(request: Request) {
-  await prisma.todo.deleteMany(); // delete * from to do
+export async function GET(request: Request) { 
+
+  await prisma.todo.deleteMany(); // delete * from todo
 
   await prisma.todo.createMany({
     data: [
-      { description: "Soul Stone", complete: true },
-      { description: "Power Stone" },
-      { description: "Time Stone" },
-      { description: "Space Stone" },
-      { description: "Reality Stone" },
-    ],
-  });
+      { description: 'Piedra del alma', complete: true },
+      { description: 'Piedra del poder' },
+      { description: 'Piedra del tiempo' },
+      { description: 'Piedra del espacio' },
+      { description: 'Piedra del realidad' },
+    ]
+  })
+ 
+  
 
-  return NextResponse.json({ message: "Seed Executed" });
+  return NextResponse.json({ message: 'Seed Executed' });
 }
